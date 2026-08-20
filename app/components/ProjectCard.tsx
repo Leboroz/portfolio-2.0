4f82ebc621459b18c553e230d0ec1e430f3ca225
leonardo <albornozleo95@gmail.com>
2026-08-20 19:46:52 -0400

    Finish Project cards

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,9 +1,10 @@
 interface ProjectCardProps {
   title: string;
   summary: string;
+  techStack: string[];
 }
 
-export const ProjectCard = ({ title, summary }: ProjectCardProps) => {
+export const ProjectCard = ({ title, summary, techStack }: ProjectCardProps) => {
   return (
     <article className="rounded-2xl bg-surface flex flex-col gap-3 flex-1 p-5">
       <div className="
@@ -18,7 +19,14 @@ export const ProjectCard = ({ title, summary }: ProjectCardProps) => {
 
       </div>
       <h3 className="font-kode-mono font-bold text-xl">{title}</h3>
-      <p className="text-muted">{summary}</p>
+      <p className="
+        text-muted
+        min-h-[60px] 
+        h-[70px] 
+        overflow-hidden 
+        line-clamp-3
+        ">{summary}</p>
+      <span className="text-terminal-green font-kode-mono">{techStack.join(' • ')}</span>
 
     </article>
   )
