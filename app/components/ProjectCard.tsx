53f7819c82ece0a23ecc0314b8f21a097d9da31a
leonardo <albornozleo95@gmail.com>
2026-08-21 02:24:25 -0400

    Finish project cards

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,22 +1,26 @@
+import { PrimaryButton } from "./PrimaryButton";
+
 interface ProjectCardProps {
   title: string;
   summary: string;
   techStack: string[];
+  sourceCode?: string;
+  liveUrl?: string;
 }
 
-export const ProjectCard = ({ title, summary, techStack }: ProjectCardProps) => {
+export const ProjectCard = ({ title, summary, techStack, sourceCode, liveUrl }: ProjectCardProps) => {
   return (
     <article className="flex flex-1 flex-col gap-3 rounded-2xl bg-surface p-5">
       <div className="
           flex 
           h-[130px] 
           items-center 
-          justify-center
           rounded-2xl
           bg-gradient-to-br
           from-[#35491B] via-[#283518] to-[#171F13]
+          ps-5
         ">
-
+        <img src='/logo/react.png' alt="react logo" />
       </div>
       <h3 className="font-kode-mono text-xl font-bold">{title}</h3>
       <p className="
@@ -26,6 +30,10 @@ export const ProjectCard = ({ title, summary, techStack }: ProjectCardProps) => 
         overflow-hidden 
         text-muted
         ">{summary}</p>
+      <div className="flex gap-3">
+        {liveUrl && <a href={liveUrl} target="_blank" rel="noreferrer"><PrimaryButton type="button" text="LIVE" /></a>}
+        {sourceCode && <a href={sourceCode} target="_blank" rel="noreferrer"><PrimaryButton type="button" text="SOURCE" /></a>}
+      </div>
       <span className="font-kode-mono text-terminal-green">{techStack.join(' • ')}</span>
 
     </article>
