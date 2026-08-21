45014d4b2c521cbb858da06ec045541f531367a6
leonardo <albornozleo95@gmail.com>
2026-08-21 02:45:06 -0400

    Upload google analitycs

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.VITE_GA_MEASUREMENT_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` 
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.VITE_GA_MEASUREMENT_ID}');
+          `}}
+        >
+        </script>
         <Meta />
         <Links />
       </head>
